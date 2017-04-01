bf8a013e7b50813fba34474d1d33c2c2ecf113fa
BurningWish <Q.Ji2@newcastle.ac.uk>
2017-04-02 00:36:11 +0800

    fix secret_key

--- catalogs/__init__.py
+++ catalogs/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
+app.secret_key = 'super_secret_key'
 
 import catalogs.views
 
 if __name__ == '__main__':
-    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=80)
